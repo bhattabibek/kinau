9bd9438d514b9f75dc398920e0ef81ee4d314ff9
Bibek Bhatta <bhattabbk1@gmail.com>
2025-05-18 12:38:39 +0545

    chore:added the app router and folder structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Link from 'next/link';
+
 
 export default function HomePage() {
   return (
-    <main>
-      <h1>Home Page</h1>
-      <Link href="/about">Go to About Page</Link>
-    </main>
+   <>
+   </>
   );
+  
 }
