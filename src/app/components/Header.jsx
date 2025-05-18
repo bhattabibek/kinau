9583d9550249d07cc496d6f967395b2df3ecfbee
Bibek Bhatta <bhattabbk1@gmail.com>
2025-05-18 13:43:55 +0545

    Feat:added sticky navbar and bentogrid

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { FaOpencart } from "react-icons/fa";
+import { CiLogin } from "react-icons/ci";
+import { CiUser } from "react-icons/ci";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+  
+       <div className='flex justify-evenly p-6 items-center bg-[#fffdfd] shadow-xl flex-wrap sticky top-0 z-50'>
+        <h1 className='text-5xl inline-flex'>KINAU <FaOpencart /></h1>
+        <div> <ul  className='flex flex-row gap-6 text-amber-700'>
+          <li>Home</li>
+          <li>About</li>
+          <li>Products</li>
+          <li>Account</li>
+          <li>Contact</li>
+        </ul></div>
+       <div className='inline-flex gap-4 '>
+       <button className='bg-[#136892] text-white p-3 px-5 rounded-xl'><CiLogin className='inline-flex text-center' size={25} />Login</button>
+     
+        <button className='bg-[#0c0c0c] text-white p-3 px-5 rounded-xl'><CiUser className='inline-flex text-center ' size={25}/>Register</button>
+       </div>
+       </div>
+     
+    </>
+ 
   )
 }
 
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Header from "./components/Header";
+import BentoGrid from "./components/BentoGrid";
 
 export const metadata = {
   title: "Create Next App",
@@ -18,12 +10,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+   <>
+       <html lang="en">
+       <body>
+      <Header/>
+      <BentoGrid/>
+      <main>{children}</main>
       </body>
-    </html>
+      </html>
+      </>
   );
 }
